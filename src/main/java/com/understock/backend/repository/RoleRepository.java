fa0af87c4635411c4469b9647314a09dcf21fd29
Zia <ziasyed2000@gmail.com>
2022-09-08 18:52:17 -0400

    added repositories for Role & User data models

---
+++ src/main/java/com/understock/backend/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.understock.backend.repository;
+
+import com.understock.backend.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
