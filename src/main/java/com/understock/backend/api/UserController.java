5e84df10872f9d800527d63daca3991909fbf271
Zia <ziasyed2000@gmail.com>
2022-09-08 19:59:17 -0400

    implemented the controller for /api routes and api/users to test

---
+++ src/main/java/com/understock/backend/api/UserController.java
@@ -0,0 +1,23 @@
+package com.understock.backend.api;
+
+import com.understock.backend.models.User;
+import com.understock.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>>getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+}
