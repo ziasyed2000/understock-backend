915dc9d9e4a9be754c06a7e75f1520aba7b9890a
Zia <ziasyed2000@gmail.com>
2022-09-08 18:45:29 -0400

    init commit - restarted a new base spring application

---
+++ src/main/java/com/understock/backend/UnderstockApplication.java
@@ -0,0 +1,13 @@
+package com.understock.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnderstockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/understock/backend/UnderstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.understock.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
