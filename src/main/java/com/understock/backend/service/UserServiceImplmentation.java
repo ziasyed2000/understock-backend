38931492d26bc5c2ad0f4ad29c9fb2d52b30cde0
Zia <ziasyed2000@gmail.com>
2022-09-08 19:03:42 -0400

    implemented the functions defined to access/save users

--- src/main/java/com/understock/backend/service/UserServiceImplmentation.java
+++ src/main/java/com/understock/backend/service/UserServiceImplmentation.java
@@ -5,37 +5,44 @@ import com.understock.backend.models.User;
 import com.understock.backend.repository.RoleRepository;
 import com.understock.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
+@Slf4j
 public class UserServiceImplmentation implements UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+
     @Override
     public User saveUser(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
-        return null;
+        return roleRepository.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
-
+        User user = userRepository.findByUsername(username);
+        Role role = roleRepository.findByName(roleName);
+        user.getRoles().add(role);
     }
 
     @Override
     public User getUser(String username) {
-        return null;
+        return userRepository.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
-        return null;
+        return userRepository.findAll();
     }
 }
