9faed05fe5b42ec9d0ac867d3f19cbcef504807f
Zia <ziasyed2000@gmail.com>
2022-09-10 23:46:40 -0400

    undo of add table name

--- src/main/java/com/understock/backend/models/User.java
+++ src/main/java/com/understock/backend/models/User.java
@@ -12,7 +12,6 @@ import java.util.Collection;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
