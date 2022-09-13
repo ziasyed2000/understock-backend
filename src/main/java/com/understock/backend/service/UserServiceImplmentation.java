330473c4483440b1757df91fc4b585a18554064f
Zia <ziasyed2000@gmail.com>
2022-09-12 22:17:27 -0400

    implemented loadByUsername to find and return the authenticated user for Spring boot login

--- src/main/java/com/understock/backend/service/UserServiceImplmentation.java
+++ src/main/java/com/understock/backend/service/UserServiceImplmentation.java
@@ -6,18 +6,44 @@ import com.understock.backend.repository.RoleRepository;
 import com.understock.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImplmentation implements UserService {
+public class UserServiceImplmentation implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            log.error("User not found in the DB.");
+            throw new UsernameNotFoundException("User not found in the DB.");
+        } else {
+            log.info("User - {} found in the DB.", username);
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
 
     @Override
     public User saveUser(User user) {
