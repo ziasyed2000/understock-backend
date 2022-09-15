0f74140d2d298298d2376873e16ca863bbf48854
Zia <ziasyed2000@gmail.com>
2022-09-14 21:57:55 -0400

    added email support

--- src/main/java/com/understock/backend/models/User.java
+++ src/main/java/com/understock/backend/models/User.java
@@ -17,6 +17,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String email;
     private String username;
     private String password;
 
