dff1791f92b308b695524e64a9723639a96a641a
Zia <ziasyed2000@gmail.com>
2022-09-19 21:36:31 -0400

    code optimizations/unused imports removed + comment updates

--- src/main/java/com/understock/backend/models/Role.java
+++ src/main/java/com/understock/backend/models/Role.java
@@ -1,16 +1,23 @@
 package com.understock.backend.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/*
+ * Model to define data required for the Role to be
+ * stored inside the database. The ID is generated using
+ * @GeneratedValue from the JPA library.
+ *
+ * The Constructor is generated automatically using
+ * the Lombok library that reduces boilerplate code.
+ */
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Role {
--- src/main/java/com/understock/backend/models/User.java
+++ src/main/java/com/understock/backend/models/User.java
@@ -1,12 +1,10 @@
 package com.understock.backend.models;
 
 import lombok.*;
-import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Objects;
 
 /*
 * Model to define data required for the User to be
@@ -19,14 +17,9 @@ import java.util.Objects;
 @Entity
 @Getter
 @Setter
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    /*
-    * -- Work In Progress --
-    * Additional variables will be added later when needed.
-    */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -44,17 +37,4 @@ public class User {
     */
     @ManyToMany(fetch = FetchType.EAGER)
     private Collection<Role> roles = new ArrayList<>();
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        User user = (User) o;
-        return id != null && Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
