9984c393a9c2a6ef04e78deaac751c2efdcef5cd
Zia <ziasyed2000@gmail.com>
2022-09-19 20:20:26 -0400

    added comments

--- src/main/java/com/understock/backend/repository/UserRepository.java
+++ src/main/java/com/understock/backend/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.understock.backend.repository;
 import com.understock.backend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * The repository for a data model, allows CRUD functionality.
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
