36385ad5ca347d22857c9e49722bbcc76cc52733
Zia <ziasyed2000@gmail.com>
2022-09-21 00:43:18 -0400

    added comments

--- src/main/java/com/understock/backend/repository/RoleRepository.java
+++ src/main/java/com/understock/backend/repository/RoleRepository.java
@@ -3,6 +3,9 @@ package com.understock.backend.repository;
 import com.understock.backend.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * The repository for a data model, allows CRUD functionality.
+ */
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
