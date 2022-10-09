0e765f6bd8955e9d5f7f4ba7e2d771ea08351dea
Zia <ziasyed2000@gmail.com>
2022-10-08 23:15:36 -0400

    tiny clean up

--- src/main/java/com/understock/backend/service/UserServiceImplementation.java
+++ src/main/java/com/understock/backend/service/UserServiceImplementation.java
@@ -101,9 +101,7 @@ public class UserServiceImplementation implements UserService, UserDetailsServic
         }
 
         Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-        user.getRoles().forEach(role -> {
-            authorities.add(new SimpleGrantedAuthority(role.getName()));
-        });
+        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
 
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
